e2f2f9fac0b338a5995384ac56ea6d8898216205
Kevin M. Butler <kbutler1@gmail.com>
2025-02-08 16:19:57 -0500

    Removed swagger

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
     ]
   },
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        process: require.resolve('process/browser'),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
