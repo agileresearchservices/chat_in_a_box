7158536586a9f0e66ced6384167d320c63a6cff4
Kevin M. Butler <kbutler1@gmail.com>
2025-03-10 16:16:24 -0400

    Updates

--- external_services/reranker-service/start.sh
+++ external_services/reranker-service/start.sh
@@ -2,6 +2,6 @@
 # Startup script for the reranker FastAPI service using uvicorn
 
 # Activate virtual environment from the project root
-source ../.venv/bin/activate
+source ../../.venv/bin/activate
 
 uvicorn main:app --host 0.0.0.0 --port 8005
